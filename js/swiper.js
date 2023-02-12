740aa062c57383618b62716356836d54446ad275
Alexandr <lipanov162@gmail.com>
2023-02-12 18:58:11 +0300

    refactoring pre release

--- js/swiper.js
+++ js/swiper.js
@@ -5,6 +5,14 @@ const swiperTeacher = new Swiper('.swiper-teachers', {
     grabCursor: true,
    // width: 300,
    loopPreventsSliding: true,
+   navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
     autoplay: {
       delay: 3300,
     },
